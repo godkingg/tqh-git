8067ab094c085a9f27edb88f1731fe7f024d4618
godkingg <161006882+godkingg@users.noreply.github.com>
2024-02-23 14:29:00 +0700

    Create tq.py

---
+++ tq.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import altair as alt
+import streamlit as st
+# Defining random Values for Chart
+df = pd.DataFrame(
+    np.random.randn(10, 2),
+    columns=['a', 'b'])
+# Defining Chart
+chart = alt.Chart(df).mark_bar().encode(
+    x='a', y='b', tooltip=['a', 'b'])
+# Defining Chart in write() function
+st.write(chart)
